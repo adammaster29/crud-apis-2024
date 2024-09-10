33271d5e8a6ea9bc0b14b252938195cc5a77e7b6
ADAM <adammaster29@gmail.com>
2024-09-10 09:34:24 -0500

    segundo commit

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.delete('/vehiculos/:id', async (req, res) => {
 
 
 //  const port = 3000;    //puerto local
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log(`Conectado al puerto: ${port}`);
 });
